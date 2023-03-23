78c71f8d683d931d1ef3d5beeebeff979a393ef8
Ivan Radko <mr.radkoivan@gmail.com>
2023-03-23 16:59:29 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,48 @@
+const ref = {
+  controls: document.querySelector("#controls"),
+  create: document.querySelector("[data-create]"),
+  destroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const divArray = [];
+  let item = "";
+  let size = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    item = document.createElement("div");
+    item.style.backgroundColor = getRandomHexColor();
+    item.style.width = size + "px";
+    item.style.height = size + "px";
+    size += 10;
+    divArray.push(item);
+  }
+
+  return ref.boxes.append(...divArray);
+}
+
+function destroyBoxes() {
+  ref.boxes.innerHTML = "";
+}
+
+ref.controls.firstElementChild.addEventListener("blur", onInput);
+ref.destroy.addEventListener("click", onClickDestroy);
+
+function onInput(evt) {
+  let value = evt.currentTarget.value;
+  ref.create.addEventListener("click", onCreate);
+  function onCreate() {
+    createBoxes(value);
+    ref.create.removeEventListener("click", onCreate);
+  }
+}
+
+function onClickDestroy() {
+  destroyBoxes();
 }
